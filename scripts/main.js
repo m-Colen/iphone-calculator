02afd900c8d9aabbd5ba19fd294c1926994b6e56
m-colen <mzcolen@gmail.com>
2022-08-16 22:24:03 -0500

    Refactor main.js & update sizing
    Refactored main.js so that the click and key events share the same function.
    
    Also, updated sizing to make it look better on mobile.

--- scripts/main.js
+++ scripts/main.js
@@ -36,130 +36,74 @@ const updateDisplay = (displayElem, value) => {
 Event listeners
 ***/
 
+// Reads key action
+const keyActions = (key) => {
+  if (currentArray.length >= 7) {
+    currentArray.shift();
+  }
+  switch (key) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      key = parseInt(key);
+      currentArray = Display.logInput(key, currentArray);
+      currentValue = Display.joinInput(currentArray);
+      updateDisplay(display, currentValue);
+      break;
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+      firstNum = Display.joinInput(currentArray);
+      currentOperator = key;
+      currentArray = Display.clearDisplay(currentArray);
+      updateDisplay(display, key);
+      break;
+    case "clear":
+      currentArray = Display.clearDisplay(currentArray);
+      currentValue = Display.joinInput(currentArray);
+      updateDisplay(display, currentValue);
+      break;
+    case "negative":
+      let toggled = Calculate.toggleNegative(currentValue);
+      currentValue = toggled;
+      currentArray = [currentValue];
+      updateDisplay(display, currentValue);
+      break;
+    case "percent":
+      currentArray = [Calculate.convertPercent(currentValue)];
+      currentValue = Display.joinInput(currentArray);
+      updateDisplay(display, currentValue);
+      break;
+    case "=":
+      currentValue = Calculate.result(firstNum, currentOperator, currentValue);
+      currentArray = [currentValue];
+      updateDisplay(display, currentValue);
+      break;
+    case ".":
+      currentArray = Calculate.addDecimal(currentArray);
+      currentValue = Display.joinInput(currentArray);
+      display.innerHTML = `${currentValue}.`;
+  }
+};
+
+// Click listener
 allButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    e.preventDefault();
     let key = e.target.value;
-    if (currentArray.length >= 7) {
-      currentArray.shift();
-    } else {
-      switch (key) {
-        case "0":
-        case "1":
-        case "2":
-        case "3":
-        case "4":
-        case "5":
-        case "6":
-        case "7":
-        case "8":
-        case "9":
-          key = parseInt(e.target.value);
-          currentArray = Display.logInput(key, currentArray);
-          currentValue = Display.joinInput(currentArray);
-          updateDisplay(display, currentValue);
-          break;
-        case "/":
-        case "*":
-        case "-":
-        case "+":
-          firstNum = Display.joinInput(currentArray);
-          currentOperator = key;
-          currentArray = Display.clearDisplay(currentArray);
-          updateDisplay(display, key);
-          break;
-        case "clear":
-          currentArray = Display.clearDisplay(currentArray);
-          currentValue = Display.joinInput(currentArray);
-          updateDisplay(display, currentValue);
-          break;
-        case "negative":
-          let toggled = Calculate.toggleNegative(currentValue);
-          currentValue = toggled;
-          currentArray = [currentValue];
-          updateDisplay(display, currentValue);
-          break;
-        case "percent":
-          currentArray = [Calculate.convertPercent(currentValue)];
-          currentValue = Display.joinInput(currentArray);
-          updateDisplay(display, currentValue);
-          break;
-        case "=":
-          currentValue = Calculate.result(
-            firstNum,
-            currentOperator,
-            currentValue
-          );
-          currentArray = [currentValue];
-          updateDisplay(display, currentValue);
-          break;
-        case ".":
-          currentArray = Calculate.addDecimal(currentArray);
-          currentValue = Display.joinInput(currentArray);
-          display.innerHTML = `${currentValue}.`;
-      }
-    }
+    keyActions(key);
   });
 });
 
+// Key listener
 window.addEventListener("keyup", (e) => {
   let key = e.key;
-  if (currentArray.length >= 7) {
-    currentArray.shift();
-  } else {
-    switch (key) {
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        key = parseInt(e.key);
-        currentArray = Display.logInput(key, currentArray);
-        currentValue = Display.joinInput(currentArray);
-        updateDisplay(display, currentValue);
-        break;
-      case "/":
-      case "*":
-      case "-":
-      case "+":
-        firstNum = Display.joinInput(currentArray);
-        currentOperator = key;
-        currentArray = Display.clearDisplay(currentArray);
-        updateDisplay(display, key);
-        break;
-      case "Delete":
-        currentArray = Display.clearDisplay(currentArray);
-        currentValue = Display.joinInput(currentArray);
-        updateDisplay(display, currentValue);
-        break;
-      case "%":
-        currentValue = Calculate.convertPercent(currentValue);
-        currentArray = [currentValue];
-        updateDisplay(display, currentValue);
-        break;
-      case "Enter":
-        currentValue = Calculate.result(
-          firstNum,
-          currentOperator,
-          currentValue
-        );
-        currentArray = [currentValue];
-        updateDisplay(display, currentValue);
-        break;
-      case ".":
-        currentArray = Calculate.addDecimal(currentArray);
-        currentValue = Display.joinInput(currentArray);
-        display.innerHTML = `${currentValue}.`;
-        break;
-      case "Backspace":
-        currentArray = Display.backspace(currentArray);
-        currentValue = Display.joinInput(currentArray);
-        updateDisplay(display, currentValue);
-    }
-  }
+  keyActions(key);
 });
